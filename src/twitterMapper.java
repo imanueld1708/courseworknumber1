809798981784382439e57b79ab776eea8f113538
imanueld1708 <imanuel.drexel@outlook.com>
2017-10-28 23:26:43 +0100

    ubah jadi value.getText()

--- src/twitterMapper.java
+++ src/twitterMapper.java
@@ -8,7 +8,7 @@ public class twitterMapper extends Mapper<Object, Text, Text, IntWritable> {
     private final IntWritable one = new IntWritable(1);
     private Text data = new Text();
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-      String[] fields = value.toString().split(";");
+      String[] fields = value.getText().toString().split(";");
         data.set(key.toString());
         one.set(fields[2].toString().length());
           context.write(data, one);
